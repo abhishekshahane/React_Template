b56aeecbd51183352f5569cc940fda865a0397b9
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-06 14:31:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM  from "react-dom"
+function MyInfo(){
+return <div> 
+	 <h1>Abhishek</h1>
+	 <p>safdsafdasfdsfads</p>
+	 <ol>
+	 <li>fdgfd</li>
+	 <li>dsfdsa</li>
+	 <li>dgssaf</li>
+	 </ol>
+      </div>
+}
+ReactDOM.render(<MyInfo/>,document.getElementById("root"))
