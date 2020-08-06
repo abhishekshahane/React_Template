f7ea0a551281ab352a16c68bcdd845eb34eb1d87
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-06 14:32:20 +0530

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+function App(){
+    return (
+    // All your other components go here, add them in your components.
+    // Remove the line of code below, just a place-holder.
+    <h1>Hello World!</h1>
+    )
+}
+// To export this function
+export default App
