52c94d12b34f1548e650591682c0988da9b5389b
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-09-17 19:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 import React from "react"
 import ReactDOM  from "react-dom"
-function MyInfo(){
-return <div> 
-	 <h1>Abhishek</h1>
-	 <p>safdsafdasfdsfads</p>
-	 <ol>
-	 <li>fdgfd</li>
-	 <li>dsfdsa</li>
-	 <li>dgssaf</li>
-	 </ol>
-      </div>
-}
-ReactDOM.render(<MyInfo/>,document.getElementById("root"))
+ReactDOM.render(<App />,document.getElementById("root"))
